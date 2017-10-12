50bfcf0a1174294d70cb0aaf950b2e76af1c70e9
Luis de Bethencourt <luisbg@kernel.org>
2017-10-12 20:11:53 +0100

    Add option for HTML input file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
+extern crate getopts;
+
 pub mod dom;
 pub mod html;
 
-use dom::NodeType;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     println!("Lanch Petrichor\n");
 
-    let hello = dom::text(String::from("Hello world"));
+    let mut opts = getopts::Options::new();
+    opts.optopt("h", "html", "HTML document", "FILENAME");
 
-    match hello.node_type {
-            NodeType::Element(_) => (),
-            NodeType::Text(txt) => {
-                println!("{}", txt)
-            }
-        }
+    let matches = opts.parse(std::env::args().skip(1)).unwrap();
+    let str_arg = |flag: &str, default: &str| -> String {
+        matches.opt_str(flag).unwrap_or(default.to_string())
+    };
+
+    // Read input file
+    let html = read_source(str_arg("h", "examples/test.html"));
+
+    // Parsing
+    let root_node = html::parse(html);
+}
+
+fn read_source(filename: String) -> String {
+    let mut str = String::new();
+    File::open(filename).unwrap().read_to_string(&mut str).unwrap();
+    str
 }
