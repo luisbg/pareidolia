e837520beb81df8e667003278c35eaa2ded4b0a0
Luis de Bethencourt <luisbg@kernel.org>
2017-10-15 16:16:55 +0100

    Support a small subset of CSS

---
+++ src/css.rs
@@ -0,0 +1,55 @@
+pub struct Stylesheet {
+    pub rules: Vec<Rule>,
+}
+
+pub struct Rule {
+    selectors: Vec<Selector>,
+    declarations: Vec<Declaration>,
+}
+
+pub enum Selector {
+    Simple(SimpleSelector),
+}
+
+pub struct SimpleSelector {
+    tag_name: Option<String>,
+    id: Option<String>,
+    class: Vec<String>,
+}
+
+pub struct Declaration {
+    name: String,
+    value: Value,
+}
+
+pub enum Value {
+    Keyword(String),
+    Length(f32, Unit),
+    ColorValue(Color),
+    // insert more values here
+}
+
+pub enum Unit {
+    Px,
+    // insert more units here
+}
+
+pub struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+pub type Specificity = (usize, usize, usize);
+
+impl Selector {
+    pub fn specificity(&self) -> Specificity {
+        // http://www.w3.org/TR/selectors/#specificity
+        let Selector::Simple(ref simple) = *self;
+        let a = simple.id.iter().count();
+        let b = simple.class.len();
+        let c = simple.tag_name.iter().count();
+        (a, b, c)
+    }
+}
